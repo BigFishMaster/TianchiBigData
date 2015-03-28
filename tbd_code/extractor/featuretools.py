08f2c2861b8d26285b57ee51a1b5f1a2564cd427
BigFishMaster <BigFishMaster@users.noreply.github.com>
2015-03-28 22:14:59 +0800

    add addtoools
    add addtools

--- tbd_code/extractor/featuretools.py
+++ tbd_code/extractor/featuretools.py
@@ -1,1 +1,21 @@
-
+def addtools(modulename,reporterror=False):
+    try:
+        if '.' in modulename:
+            a = modulename.split('.')
+            t = __import__('.'.join(a[:-1]),fromlist=[a[-1]])
+            tempmodulename = getattr(t,a[-1])
+        else:
+            tempmodulename = __import__(modulename)
+        allloadmodules.append(tempmodulename)
+        if hasattr(tempmodulename,'doc'):
+            doc = tempmodulename.doc
+        else:
+            doc = ''
+    except ImportError,e:
+        if 'No module named' in str(e):
+            if reporterror:
+                logging.warning('fail to import %s',str(e))
+            return
+        else:
+            raise
+    runtools(tempmodulename)
