41b7d463b4f142e4679898977e9f2f90300dbf4d
BigFishMaster <yancanxiang@163.com>
2015-03-28 23:19:39 +0800

    revision:
    add calfeatures function

--- tbd_code/extractor/featureextractor.py
+++ tbd_code/extractor/featureextractor.py
@@ -1,1 +1,19 @@
-# empty head
+import logging
+import sys
+import userfeatureextractor
+import itemfeatureextractor
+import pairfeatureextractor
+
+
+def calfeatures(configfilename):
+    
+if __name__ == '__main__':
+    LOG_LEVEL = logging.DEBUG
+    logging.basicConfig(level=LOG_LEVEL,
+                format="%(levelname)s:%(name)s:%(funcName)s->%(message)s",  #logging.BASIC_FORMAT,
+                datefmt='%a, %d %b %Y %H:%M:%S')
+    if len(sys.argv)>1 :
+        func = getattr(sys.modules[__name__], sys.argv[1])
+        func(*sys.argv[2:])
+    else:
+        print >> sys.stderr,'debugsearch.py command'
