b72a183d94692c3286a47495714b9cf442ea75bc
BigFishMaster <yancanxiang@163.com>
2015-03-30 00:20:22 +0800

    info: revise featureextractor.py

--- tbd_code/extractor/featureextractor.py
+++ tbd_code/extractor/featureextractor.py
@@ -40,34 +40,16 @@ class FeatureExtractor(object):
         savename = self.conf.get(self.sectionname,self.field+".savename")
         self.savename = savename
         # global parameters: feature types
-        self.featureconfname = self.conf.get(self.sectionname, confname)
-        self.featurename = {}
-        self.featurename['user'] = self.conf.get(self.sectionname, "user.list").split(",")
-        self.featurename['item'] = self.conf.get(self.sectionname, "item.list").split(",")
-        self.featurename['pair'] = self.conf.get(self.sectionname, "pair.list").split(",")
-        self.featureindex['user'] = self.conf.get(self.sectionname, "user.index")
-        self.featureindex['item'] = self.conf.get(self.sectionname, "item.index")
-        self.featureindex['pair'] = self.conf.get(self.sectionname, "pair.index")
-       
-        self.conf.get(self.sectionname, "user.list").split(",")
-        
-        featcalusedindex = []
-        featurecalnames = []
-        self.inputimagenames = []
-        self.feacals = {}
-        self.featureinfos = []
-        for i,name in enumerate(featurenames):
-            feainfo = self.getnameconf(name)
-            index = feainfo['index']
-            if feainfo['inputimagename'] not in self.inputimagenames:
-                self.inputimagenames.append(feainfo['inputimagename'])
-            feacalkey = ( feainfo['classname'],feainfo['prefix'])
-            if feacalkey  not in self.feacals:
-                self.feacals[feacalkey]= {'cal':None,'usedindex':[index]}
-            else:
-                if index not in self.feacals[feacalkey]['usedindex']:
-                    self.feacals[feacalkey]['usedindex'].append(index)
-            self.featureinfos.append(feainfo)
+        self.featureconfname = self.conf.get(self.sectionname, "confname")
+        self.featurenames = self.conf.get(self.sectionname, "featurenames").split(",")
+        self.featurelist = {}
+        for name in self.featurenames:
+            self.featurelist[name] = self.conf.get(self.sectionname, name + ".list").split(",")
+
+        self.featureindex = {}
+        for name in self.featurenames:
+            self.featureindex[name] = self.conf.get(self.sectionname, name + ".index").split(",")
+
 # field: train, val, test
 # configfilename: used to initialize time period and feature type.
 def calfeatures(field, configfilename):
