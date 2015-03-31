5a3940f2434bff8f701767757cd73707caa96dfd
BigFishMaster <yancanxiang@163.com>
2015-03-31 22:35:34 +0800

    info: add class PFE

--- tbd_code/extractor/pairfeatureextractor.py
+++ tbd_code/extractor/pairfeatureextractor.py
@@ -2,35 +2,46 @@
 
 class PFE(object):
     def __int__(self, params):
-        period = params['user'].period
-        featurelist = params['user'].featurelist
-        inputname = params['user'].inputname
-        #click,buy,store,cart,avgtime2deadline
-        feafunc = {}
-        feafunc[''] = 
-    
+        self.period = params['user'].period
+        self.featurelist = params['user'].featurelist
+        self.featureconfname = params['user'].featureconfname
+        self.inputname = params['user'].inputname
+        self.feafunc = {}
+        # initialize feafunc:
+        self.feafunc['clickbuystorecart'] = self.clickbuystorecart
+        
         #fea 1: the distribute of click,buy,store,cart in :
         # time : last friday, weekends, Mon. to Tues: 3 types
         # dim : 4-dim distribution
         # input : metadata: {[userid],[itemid], [behavior]}
 
-    def clickbuystorecart( metadata ):
+    def clickbuystorecart( mdata ):
         
-    def extract(params):
-
-        metadata = preprocess( inputname )
+    def extract():
+        metadata = self.preprocess()
+        res = {}
+        res['userid'] = []
+        res['itemid'] = []
+        res['fea'] = []
         for key in metadata:
             mdata = metadata[key]
             mfea = []
-            for name in featurelist:
-                if name in feafunc:
-                    f = feafunc[name]()
-
+            for name in self.featurelist:
+                if name in self.feafunc:
+                    f = self.feafunc[name]( mdata )
+                    mfea += f
+            userid, itemid = key.split(',')
+            res['userid'].append(userid)
+            res['itemid'].append(itemid)
+            res['fea'].append(mfea)
+        
+        return res
+        
     # formulate data to metadate 
     # denoise
     # input original data
-    def preprocess( file ):
-        list = open(file, 'r').readlines()
+    def preprocess( ):
+        list = open(self.inputname, 'r').readlines()
         metadata = {}
         for i, item in enumerate(list):
             if i == 0: # skip the first line
